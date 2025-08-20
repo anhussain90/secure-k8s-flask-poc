5a477c4d6203272c41ebf0601a5500a34cc652fa
Anwaar Hussain <32175710+anhussain90@users.noreply.github.com>
2025-08-20 19:45:39 +0100

    Update main.tf

--- infra/terraform/modules/remote-state/main.tf
+++ infra/terraform/modules/remote-state/main.tf
@@ -46,9 +46,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "${var.project_name}-terraform-state-lock-${random_id.bucket_suffix.hex}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "${var.project_name}-terraform-state-lock-${random_id.bucket_suffix.hex}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
