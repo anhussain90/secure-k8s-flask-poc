e0cb3f627c44388dc5d181ecf5f4d387a3559b7d
Anwaar Hussain <32175710+anhussain90@users.noreply.github.com>
2025-08-22 18:10:44 +0100

    Chore: updated app

--- app/app.py
+++ app/app.py
@@ -39,5 +39,5 @@ def root():
     }), 200
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
+    port = int(os.getenv('PORT', 80))
     app.run(host='0.0.0.0', port=port, debug=False)
